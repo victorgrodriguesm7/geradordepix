5894047f1e58c59a600b92b6c000ee0e91a5ccd4
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2025-05-18 12:42:11 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { Request, Response } from 'express';
+import cors from "cors";
 import { handleGenerateQrCode } from "./pix";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (_, res) => {
     res.status(200).json({ hello: "world!" });
