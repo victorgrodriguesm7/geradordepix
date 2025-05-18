c5def2c9836ad1f6225f75b74578b7aa037b8e69
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2025-05-18 12:28:05 -0300

    feat: add serverless framework to host on aws lambda

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { Request, Response } from 'express';
+import { handleGenerateQrCode } from "./pix";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.status(200).json({ hello: "world!" });
+})
+
+app.route("/:key")
+    .get((req: Request, res: Response) => {
+        handleGenerateQrCode(req, res);
+    })
+    .post((req: Request, res: Response) => {
+        handleGenerateQrCode(req, res);
+    });
+
+export {
+    app
+};
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,6 @@
-import express from "express";
-import { Request, Response } from 'express';
-import { handleGenerateQrCode } from "./pix";
+import { app } from "./app";
 
 const port = process.env.PORT || 3000;
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (_, res) => {
-    res.status(200).json({ hello: "world!" });
-})
-
-app.route("/:key")
-    .get((req: Request, res: Response) => {
-        handleGenerateQrCode(req, res);
-    })
-    .post((req: Request, res: Response) => {
-        handleGenerateQrCode(req, res);
-    });
 
 app.listen(port, () => {
     console.log(`Starting Server on port`, port)
